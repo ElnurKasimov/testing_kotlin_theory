62736a39babdf2f9a3c2d997fcae322e1821d2ed
ElnurKasimov <ekasimov2012@gmail.com>
2024-09-20 23:21:50 +0300

    amended LastDigit

--- app/src/main/java/com/example/firstapplication/LastDigit.kt
+++ app/src/main/java/com/example/firstapplication/LastDigit.kt
@@ -4,9 +4,15 @@ import kotlin.math.abs
 
 fun getLastDigit (number : Int) = abs(number % 10)
 
+fun isRightEquation(a : Int, b : Int, c : Int) = (a * b) == c
 
 /* Do not change code below */
 fun main() {
+    val aa = readln().toInt()
+    println(getLastDigit(aa))
+
     val a = readln().toInt()
-    println(getLastDigit(a))
+    val b = readln().toInt()
+    val c = readln().toInt()
+    println(isRightEquation(a, b, c))
 }
