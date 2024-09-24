cbd6c33592c81623ae41fe4db27c125b2e01b7fa
Elnur Kasimov <ekasimov2012@gmail.com>
2024-09-24 15:46:43 +0300

    added ShiftArbitraryInArray.kt, ShiftByOneInArray.kt into SyntaxTest

---
+++ app/src/main/java/com/example/firstapplication/SyntaxTest/ShiftByOneInArray.kt
@@ -0,0 +1,17 @@
+package com.example.firstapplication.SyntaxTest
+
+fun main() {
+    val n = readln().toInt()
+    val a : MutableList<Int> = mutableListOf()
+    for(i in 1..n) {
+        a.add(readln().toInt())
+    }
+    for(i in 0 until n) {
+        if (n - 1 + i < n) {
+            print("${a[n - 1 + i]} ")
+
+        } else {
+            print("${a[i - 1]} ")
+        }
+    }
+}
