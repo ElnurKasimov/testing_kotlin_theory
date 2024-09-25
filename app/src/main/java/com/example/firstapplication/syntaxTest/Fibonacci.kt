31489f871ed2d082fdce2d4649bde37b5aaa86cb
Elnur Kasimov <ekasimov2012@gmail.com>
2024-09-25 12:39:10 +0300

    added Fibonacci.kt into SyntaxTest

---
+++ app/src/main/java/com/example/firstapplication/syntaxTest/Fibonacci.kt
@@ -0,0 +1,16 @@
+package com.example.firstapplication.syntaxTest
+
+fun main() {
+    val n = readln().toInt()
+    var t1 = 0
+    var t2 = 1
+    print("$t1, $t2")
+    var counter = 2
+    while (counter < n) {
+        var res = t1 + t2
+        print(", $res")
+        t1 = t2
+        t2 = res
+        counter++
+    }
+}
--- app/src/main/java/com/example/firstapplication/syntaxTest/NumberInputs.kt
+++ app/src/main/java/com/example/firstapplication/syntaxTest/NumberInputs.kt
@@ -1,10 +1,15 @@
 package com.example.firstapplication.syntaxTest
 
 fun main() {
-   var sum = 0
-    do {
-        val number = readln().toInt()
-        sum += number
-    } while (number != 0)
-    println(sum)
+//   var sum = 0
+//    do {
+//        val number = readln().toInt()
+//        sum += number
+//    } while (number != 0)
+//    println(sum)
+    var number = readln().toInt()
+    while(number >0) {
+        println(number)
+        number--
+    }
 }
