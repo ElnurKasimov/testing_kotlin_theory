aff2c1158cbc6c3d2440c3aa193571cd0e3f23ef
ElnurKasimov <ekasimov2012@gmail.com>
2024-09-28 22:13:29 +0300

    Add BoxToString.kt into dataClassTest

--- app/src/main/java/com/example/firstapplication/dataClassTest/Boxes.kt
+++ app/src/main/java/com/example/firstapplication/dataClassTest/Boxes.kt
@@ -1,13 +1,13 @@
 package com.example.firstapplication.dataClassTest
 
-data class Box(val height: Int, val length: Int, val width: Int)
+data class Boxx(val height: Int, val length: Int, val width: Int)
 
 fun main() {
     val height = readln().toInt()
     val length1 = readln().toInt()
     val length2 = readln().toInt()
     val width = readln().toInt()
-    val box1 = Box(height, length1, width)
+    val box1 = Boxx(height, length1, width)
     val box2 = box1.copy(length = length2)
     println(box1)
     println(box2)
