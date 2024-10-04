f3ccc02b00d2caaab69ffa0f4188431509fa84cf
Elnur Kasimov <ekasimov2012@gmail.com>
2024-10-04 13:37:24 +0300

    Add ByteTimer.kt into classTest

---
+++ app/src/main/java/com/example/firstapplication/classTest/ByteTimer.kt
@@ -0,0 +1,18 @@
+package com.example.firstapplication.classTest
+
+fun main() {
+    val timerValue = readln().toInt()
+    val timer = ByteTimer(timerValue)
+    println(timer.time)
+}
+
+class ByteTimer(_time: Int) {
+    var time: Int = 0
+    init {
+        time = when {
+            _time < -128 -> - 128
+            _time > 127 -> 127
+            else -> _time
+        }
+    }
+}
