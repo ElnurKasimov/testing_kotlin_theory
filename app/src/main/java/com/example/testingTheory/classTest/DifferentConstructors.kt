11bc92f6f74a819f7be19879518603bb6dd951d0
Elnur Kasimov <ekasimov2012@gmail.com>
2024-10-15 14:50:18 +0300

    Amend DifferentConstructors.kt into classTest

--- app/src/main/java/com/example/testingTheory/classTest/DifferentConstructors.kt
+++ app/src/main/java/com/example/testingTheory/classTest/DifferentConstructors.kt
@@ -5,11 +5,25 @@ class Mug {
     constructor(coffee: Coffee)
 }
 
-enum class Tea {GREEN_TEA, LACK_TEA, RED_TEA}
-enum class Coffee {LIGHT, MEDIUM, STRONG}
+enum class Tea: Beverage {GREEN_TEA, LACK_TEA, RED_TEA}
+
+enum class Coffee: Beverage {LIGHT, MEDIUM, STRONG}
+
+class Glass (val beverage: Beverage) {
+    fun drink() {
+        println("Drinking $beverage")
+    }
+}
+
+interface Beverage {}
+
 
 fun main() {
     val mug1 = Mug(Tea.LACK_TEA)
     val mug2 = Mug(Coffee.STRONG)
+    val glass1 = Glass(Tea.RED_TEA)
+    val glass2 = Glass(Coffee.MEDIUM)
+    glass1.drink()
+    glass2.drink()
 }
 
