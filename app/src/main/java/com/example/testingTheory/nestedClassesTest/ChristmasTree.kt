02cf127cf1154993685c0dce7d55beed3aff9ca6
Elnur Kasimov <ekasimov2012@gmail.com>
2024-10-21 16:29:46 +0300

    Add ChristmasTree.kt into nestedClassesTest, NullChecking.kt into syntaxTest

---
+++ app/src/main/java/com/example/testingTheory/nestedClassesTest/ChristmasTree.kt
@@ -0,0 +1,21 @@
+package com.example.testingTheory.nestedClassesTest
+
+class ChristmasTree(var color: String) {
+
+    fun putTreeTopper(color: String) {
+        val treeTopper = TreeTopper(color)
+        treeTopper.sparkle()
+    }
+
+    inner class TreeTopper(var color: String) {
+
+        fun sparkle() {
+            println("The sparkling $color tree topper looks stunning on the ${this@ChristmasTree.color} Christmas tree!")
+        }
+    }
+}
+
+fun main() {
+    val tree = ChristmasTree("green")
+    tree.putTreeTopper("silver")
+}
---
+++ app/src/main/java/com/example/testingTheory/syntaxTest/NullChecking.kt
@@ -0,0 +1,12 @@
+package com.example.testingTheory.syntaxTest
+
+fun main() {
+    val number = readln().toInt()
+    println(round(number) ?: 0)
+}
+
+// do not change function below
+
+fun round(number: Int): Int? {
+    return if (number >= 1000) null else number
+}
