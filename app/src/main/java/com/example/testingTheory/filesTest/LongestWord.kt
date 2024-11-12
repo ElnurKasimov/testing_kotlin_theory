51e3b1b75daff70b74f4c7d374a711ea237367f0
Elnur Kasimov <ekasimov2012@gmail.com>
2024-11-12 13:03:48 +0200

    Amend LongestWord.kt

--- app/src/main/java/com/example/testingTheory/filesTest/LongestWord.kt
+++ app/src/main/java/com/example/testingTheory/filesTest/LongestWord.kt
@@ -8,16 +8,12 @@ fun main() {
     val file = File(fileName)
     if(file.exists()) {
         val words = file.readLines()
-        /*
-        var maxLength = words[0].length
-        for (word in words) {
-            if(word.length > maxLength) maxLength = word.length
-         println(maxLength)
-        }*/
-
-//        println(words.map{ w -> w.length }.max())
-
         println(words.maxOfOrNull { it.length })
+
+//        val words = file.readText().split("\\W+".toRegex())
+//        println(words.count())
+
+
 
     } else {
         println("There is no file in the directory")
